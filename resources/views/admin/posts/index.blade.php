cb35e34766830db88cf0b11dc385f4ba008e7d91
RobyLedda94 <robyledda01@yahoo.it>
2024-10-04 19:28:07 +0200

    [UPD] definita destroy, da definire conferma di cancellazione

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -4,8 +4,10 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-12">
-                <h3 class="my-3">Tabella</h3>
-                <a href="{{ route('admin.posts.create')}}" class="btn btn-sm btn-primary">Aggiungi nuovo elemento</a>
+                <h3 class="my-3">Tabella dei Post</h3>
+                <a href="{{ route('admin.posts.create') }}" class="btn btn-sm btn-primary">
+                    <i class="fas fa-plus"></i> Aggiungi nuovo elemento
+                </a>
                 <hr>
             </div>
             <div class="col-12">
@@ -26,13 +28,13 @@
                                 <td>{{ $post->slug }}</td>
                                 <td>
                                     <div class="d-flex">
-                                        <a href="{{ route('admin.posts.show', ['post' => $post->id])}}" class="me-2 btn btn-sm btn-primary">
+                                        <a href="{{ route('admin.posts.show', ['post' => $post->id]) }}" class="me-2 btn btn-sm btn-primary" aria-label="Visualizza post">
                                             <i class="fas fa-eye"></i>
                                         </a>
-                                        <a href="{{ route('admin.posts.edit', ['post' => $post->id])}}" class="me-2 btn btn-sm btn-warning">
+                                        <a href="{{ route('admin.posts.edit', ['post' => $post->id]) }}" class="me-2 btn btn-sm btn-warning" aria-label="Modifica post">
                                             <i class="fas fa-edit"></i>
                                         </a>
-                                        <form action="{{ route('admin.posts.destroy', ['post' => $post->id]) }}">
+                                        <form action="{{ route('admin.posts.destroy', ['post' => $post->id]) }}" method="POST" class="d-inline">
                                             @csrf
                                             @method('DELETE')
                                             <button type="submit" class="btn btn-sm btn-danger delete">Cancella</button>
